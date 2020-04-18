93b267570ed267e369aa71806e9de809d5aa9062
Scott Watkins <scott@tailwindapp.com>
2020-04-18 10:16:24 -0500

    configure storybook with typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,22 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  addons: [
+      '@storybook/addon-actions',
+      '@storybook/addon-links',
+      {
+        name: '@storybook/preset-typescript',
+        options: {
+          tsLoaderOptions: {
+            configFile: path.resolve(__dirname, './tsconfig.json'),
+          },
+          forkTsCheckerWebpackPluginOptions: {
+            colors: false, // disables built-in colors in logger messages
+          },
+          include: [path.resolve(__dirname, '../src')],
+          transpileManager: true,
+        },
+      },
+  ],
 };
