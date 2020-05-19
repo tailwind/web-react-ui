ec0b0c7e04a583a7c5f87eef5cc60725893d3a3b
Scott Watkins <scott@tailwindapp.com>
2020-05-19 07:53:43 -0500

    add rollup

---
+++ rollup.config.ts
@@ -0,0 +1,43 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
+import scss from "rollup-plugin-scss";
+import json from "@rollup/plugin-json";
+import pkg from "./package.json";
+
+const fs = require("fs");
+
+export default {
+    input: "src/lib/index.ts",
+    output: [
+        {
+            file: pkg.main,
+            format: "cjs",
+        },
+        {
+            file: pkg.module,
+            format: "es",
+        },
+    ],
+    external: [
+        "react",
+        "moment",
+        "moment-timezone"
+    ],
+    plugins: [
+        resolve(),
+        json(),
+        commonjs({
+            include: 'node_modules/**',
+            namedExports: {
+                'node_modules/react/react.js': ['Children', 'Component', 'PropTypes', 'createElement'],
+                'node_modules/react-dom/index.js': ['render', 'findDOMNode', 'unmountComponentAtNode'],
+                'node_modules/react-is/index.js': ['isFragment', 'ForwardRef'],
+            }
+        }),
+        scss(),
+        typescript({
+            useTsconfigDeclarationDir: true,
+        }),
+    ],
+};
